534154e37c98050859ae34007818e13a0ba7c796
ccmite01 <ccmite01@gmail.com>
2019-12-24 09:37:05 +0900

    20191224

--- start.sh
+++ start.sh
@@ -56,9 +56,9 @@ touch /opt/minecraft/ssh/authorized_keys
 chown root:root /opt/minecraft/ssh/authorized_keys
 chmod 600 /opt/minecraft/ssh/authorized_keys
 /etc/init.d/ssh start
-sed -i 's/server-port=25565/server-port=25585/g' /opt/minecraft/test/server.properties
+sed -i 's/server-port=25566/server-port=25585/g' /opt/minecraft/test/server.properties
 sed -i 's/rcon.port=25575/rcon.port=25595/g' /opt/minecraft/test/server.properties
-sed -i 's/query.port=25565/query.port=25585/g' /opt/minecraft/test/server.properties
+sed -i 's/query.port=25566/query.port=25585/g' /opt/minecraft/test/server.properties
 screen -S mcs su -s /bin/bash - www-data -c "export $TZ=JST-9; export $LANG=ja_JP.UTF-8; cd /opt/minecraft/${MC_INSTANCE_NAME}; /usr/bin/java -server -Xms${MC_RAM} -Xmx${MC_RAM} -XX:MetaspaceSize=512M -XX:+UseG1GC -XX:+UseStringDeduplication -XX:+DisableExplicitGC -XX:+UseCompressedOops -XX:+OptimizeStringConcat -XX:MaxGCPauseMillis=50 -XX:+UseTLAB -XX:ParallelGCThreads=${MC_CPU_CORE} -jar papermc-${MC_VERSION}-${MC_PAPER_BUILD}.jar nogui"
 
 /usr/bin/printenv | awk '{print "export " $1}' > /env.sh
